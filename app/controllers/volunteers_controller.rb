178fa710b0d4a4f55a4a1c7687334fa340c18501
Michael Dion <michaeldion@Michaels-MacBook-Pro.local>
2014-08-14 16:13:22 -0400

    Volunteers Crud Complete

--- app/controllers/volunteers_controller.rb
+++ app/controllers/volunteers_controller.rb
@@ -1,5 +1,7 @@
 class VolunteersController < ApplicationController
-  
+  before_action :find_group
+  before_action :find_volunteer, only: [:show, :edit, :update, :destroy]
+
   def index
     @volunteers = Volunteer.all
   end
@@ -8,16 +10,41 @@ class VolunteersController < ApplicationController
   end
 
   def new
+    @volunteer = Volunteer.new
+  end
+
+  def create
+    @volunteer = @group.volunteers.create volunteer_params
+    if @volunteer.save == true
+    redirect_to group_volunteer_path(@group, @volunteer)
+    else
+    render :new
+    end
   end
 
   def edit
   end
 
+  def update
+    @volunteer.update_attributes volunteer_params
+    redirect_to group_volunteer_path(@group, @volunteer)
+  end
+
+  def destroy
+    @volunteer.delete
+    redirect_to group_path(@group, @volunteer), method: :delete
+  end
+
   private
+
+  def find_group
+    @group = Group.find params[:group_id]
+  end
   
   def find_volunteer
     @volunteer = Volunteer.find params[:id]
   end
+
   def volunteer_params
     params.require(:volunteer).permit(:name)
   end
