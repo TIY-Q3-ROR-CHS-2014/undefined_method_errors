0d596466f8399b728c75814ec558911c231de0e8
Michael Dion <michaeldion@Michaels-MacBook-Pro.local>
2014-08-19 11:16:56 -0400

    Fixed sign-up

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,7 +8,9 @@ module ApplicationHelper
         (link_to "Edit", edit_volunteer_registration_path) +
         (link_to "Log Out", destroy_volunteer_session_path, method: :delete)
     else
-      link_to "Log In", new_volunteer_session_path
+      link_to "Log In", new_volunteer_session_path, class: 'btn btn-success btn-block'
+      link_to "Sign-Up",new_volunteer_registration_path(group_id: @group.id), class: 'btn btn-info btn-block'
+
     end
   end
 end
